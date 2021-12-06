a46f751642c77213cc976b41c4dae16a7f89614c
Mohammed Ihsanulla <mohammed.ihsanulla@yahoo.in>
2021-12-06 19:40:02 +0530

    permissions edit page has been completed with role types

--- mohammed-ihsanulla/code/server.js
+++ mohammed-ihsanulla/code/server.js
@@ -23,10 +23,10 @@ server.get("/", async (req, res) => {
       roles = responese.data;
     })
     .catch((err) => res.json(err));
-  res.render("index", { title: "Admin", roles: roles });
+  res.render("index", { title: "Admin", roles: roles, editmode: false });
 });
 
-// =========================================================================ADMIN VIEW============================================================================
+// =========================================================================MODERATOR VIEW============================================================================
 server.get("/moderator", async (req, res) => {
   var roles;
   // get availabe moderator user permissions
@@ -40,10 +40,43 @@ server.get("/moderator", async (req, res) => {
   res.render("index", {
     title: "Moderator",
     roles: roles,
+    editmode: false,
   });
 });
 
-// =========================================================================ADMIN VIEW============================================================================
+server.get("/moderator/edit", async (req, res) => {
+  var roles;
+  var permissions;
+  // get all admin user permissions
+  await axios
+    .get("http://localhost:5000/api/roles?name=moderator")
+    .then((responese) => {
+      roles = responese.data;
+    })
+    .catch((err) => res.json(err));
+
+  // get all list of permissions
+  await axios
+    .get("http://localhost:5000/api/permissions")
+    .then((responese) => {
+      permissions = responese.data;
+    })
+    .catch((err) => res.json(err));
+  res.render("index", {
+    title: "Moderator",
+    roles: roles,
+    editmode: true,
+    permissions: permissions,
+  });
+});
+
+server.patch("/moderator/edit", async (req, res) => {
+  const ispermission = { ispermission: req.body.permissions };
+  await axios.patch("http://localhost:5000/api/roles/2", ispermission);
+  res.redirect("/");
+});
+
+// =========================================================================MARKETING VIEW============================================================================
 server.get("/marketing", async (req, res) => {
   var roles;
   // get availabe admin user permissions
@@ -59,11 +92,44 @@ server.get("/marketing", async (req, res) => {
     roles: roles,
   });
 });
-// =========================================================================MARKETING VIEW============================================================================
+
+server.get("/marketing/edit", async (req, res) => {
+  var roles;
+  var permissions;
+  // get all admin user permissions
+  await axios
+    .get("http://localhost:5000/api/roles?name=marketing")
+    .then((responese) => {
+      roles = responese.data;
+    })
+    .catch((err) => res.json(err));
+
+  // get all list of permissions
+  await axios
+    .get("http://localhost:5000/api/permissions")
+    .then((responese) => {
+      permissions = responese.data;
+    })
+    .catch((err) => res.json(err));
+  res.render("index", {
+    title: "Marketing",
+    roles: roles,
+    editmode: true,
+    permissions: permissions,
+  });
+});
+
+server.patch("/marketing/edit", async (req, res) => {
+  const ispermission = { ispermission: req.body.permissions };
+  await axios.patch("http://localhost:5000/api/roles/3", ispermission);
+  res.redirect("/marketing");
+});
+// =========================================================================MEMBER VIEW============================================================================
 server.get("/member", async (req, res) => {
   res.render("index", {
     title: "Member",
     roles: [],
+    editmode: false,
   });
 });
 server.listen(5000);
